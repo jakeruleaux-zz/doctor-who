c969954d1a57156142dbd3a499e6e7e4ab658bb5
jacob ruleaux <jakeruleaux@gmail.com>
2017-08-06 16:34:34 -0700

    full display of drs list

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -3,16 +3,17 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 var listDoctors = function(medicalIssue, doctors) {
     doctors.forEach(function(doctor) {
-        console.log(listDoctors);
-$('#doctor').text("<p>" + doctor.profile.last_name + " , " + doctor.profile.first_name + "</p>" + "hi");
+        // console.log(doctor);
+$('#doctor').append("<p>" + doctor.profile.last_name + " , " + doctor.profile.first_name + "</p>" + "hi");
 });
 };
+
 
 $(document).submit(function(event) {
   event.preventDefault();
 
   var medicalIssue = $("#input").val();
   var newDoctor = new Doctor(medicalIssue);
-
+  $('#syptom').text(medicalIssue);
   newDoctor.getDoctors(medicalIssue, listDoctors);
 });
--- js/doctor.js
+++ js/doctor.js
@@ -7,7 +7,7 @@ function Doctor() {
 Doctor.prototype.getDoctors = function(medicalIssue, listDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
   .then(function(response) {
-      console.log(listDoctors);
+    //   console.log(response);
      listDoctors(medicalIssue, response.data);
   })
   .fail(function(error){
