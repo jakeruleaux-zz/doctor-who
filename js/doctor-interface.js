c0e70d70dc9414f8075f511da6fcd3e552d1a32c
jacob ruleaux <jakeruleaux@gmail.com>
2017-08-08 22:05:13 -0700

    clean up indents on js files

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,19 +1,21 @@
-var Doctor = require('./../js/doctor.js').doctorModule;
+var Doctor =    require('./../js/doctor.js').doctorModule;
 
-var listDoctors = function(medicalIssue, doctors) {
+var listDoctors = function(doctors) {
     doctors.forEach(function(doctor) {
-$('#doctor').append("<p>" + doctor.profile.last_name + " , " + doctor.profile.first_name +  " , " + doctor.profile.title + "</p>");
-});
+      $('#doctor').append("<p>" + doctor.profile.last_name + " , " + doctor.profile.first_name +  " , " + doctor.profile.title + "</p>");
+  });
 };
 
 
 $(document).ready(function() {
   $('#input-form').submit(function(event){
   event.preventDefault();
+
   var medicalIssue = $("#input").val();
   var newDoctor = new Doctor(medicalIssue);
   newDoctor.getDoctors(medicalIssue, listDoctors);
   $('#input').val(" ");
   $('#symptom').append("This hurts " + medicalIssue);
- });
+  // $(medicalIssue).empty();
+  });
 });
