16af2eac4e7f1412e254dded13096331f4954927
jacob ruleaux <jakeruleaux@gmail.com>
2017-08-10 17:15:37 -0700

    add listed addresses for drs - no display

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,19 +2,19 @@ var Doctor =    require('./../js/doctor.js').doctorModule;
 
 var listDoctors = function(medicalIssue, doctors) {
     doctors.forEach(function(doctor) {
-      $('#doctor').append("<p>" + doctor.profile.last_name + " , " + doctor.profile.first_name +  " , " + doctor.profile.title + "</p>" + <ul> + <li> + doctor.profile.practices + </li> + </ul>);
+      $('#doctor').append("<p>" + doctor.profile.last_name + " , " + doctor.profile.first_name +  " , " + doctor.profile.title + "</p>" + "<ul>" + "<li>" + doctor.practices.visit + "</li>" + "</ul>");
+      console.log(doctor.practices[0].visit_address.street);
   });
 };
 
-
 $(document).ready(function() {
   $('#input-form').submit(function(event){
-  event.preventDefault();
+    event.preventDefault();
 
-  var medicalIssue = $("#input").val();
-  var newDoctor = new Doctor(medicalIssue);
-  newDoctor.getDoctors(medicalIssue, listDoctors);
-  $('#input').val(" ");
-  $('#symptom').append("This hurts " + medicalIssue);
+    var medicalIssue = $("#input").val();
+    var newDoctor = new Doctor(medicalIssue);
+    newDoctor.getDoctors(medicalIssue, listDoctors);
+    $('#input').val(" ");
+    $('#symptom').append("This hurts " + medicalIssue);
   });
 });
